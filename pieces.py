6c2d9bf35e7665b80ead47ea583ec12450d27508
Yasin Akbarnejad <workyasin2007@gmail.com>
2026-01-03 09:28:39 +0330

    piece classes

--- pieces.py
+++ pieces.py
@@ -3,17 +3,18 @@ class Pieces():
     def __init__(self, color, position) -> None:
         self.moves = []
         self.color = color
+        inCheck = False
+        self.opponentmove = []
     def move(self,newpos):
         self.row, self.col = newpos
-        if type(self) in [Pawn]:
-            print(self.moved)
+        if type(self) in [Pawn, King]:
             self.moved = True
 class Rook(Pieces):
     def __init__(self, color, position) -> None:
         super().__init__(color,position)
         self.row, self.col = position
     def __repr__(self) -> str:
-        return f"R{self.color[0]}"
+        return f"R {self.color[0].title()}"
     def make_move(self):
         self.moves = []
         direction = [(1,0), (1, 0), (0, 1), (0, -1)]
@@ -35,59 +36,59 @@ class Bishop(Pieces):
         super().__init__(color,position)
         self.row, self.col = position
     def __repr__(self) -> str:
-        return f"B{self.color[0]}"
+        return f"B {self.color[0].title()}"
     def make_move(self):
-        moves =[]
+        self.moves = []
         direction = [(1,1), (1, -1), (-1, 1), (-1, -1)]
         for dcol, drow in direction:
             newrow, newcol = self.row +drow , self.col + dcol
             while 0<newrow<9 and 0<newcol<9:
                 boxpiece = bo.piece_name((newrow,newcol)) 
                 if boxpiece==" ":
-                    moves.append((newrow,newcol))
+                    self.moves.append((newrow,newcol))
                     newrow+=drow
                     newcol+=dcol
                 else:
-                    if boxpiece.color== self.color:
-                        moves.append((newrow,newcol))
+                    if boxpiece.color!= self.color:
+                        self.moves.append((newrow,newcol))
                     break
-        return moves
+        return self.moves
 class Knight(Pieces):
     def __init__(self, color, position) -> None:
         super().__init__(color,position)
         self.row, self.col = position
     def __repr__(self) -> str:
-        return f"N{self.color[0]}"
+        return f"N {self.color[0].title()}"
     def make_move(self):
-        moves =[]
+        self.moves =[]
         Lmove = [(2,1), (2, -1), (-2, 1), (-2, -1),(1, 2), (-1, 2), (1, -2), (-1, -2)]      
         for dcol, drow in Lmove:
             newrow, newcol = self.row +drow , self.col + dcol 
             if 0<newrow<9 and 0<newcol<9:
                 boxpiece = bo.piece_name((newrow,newcol))
                 if boxpiece==" " or boxpiece.color != self.color:
-                    moves.append((newrow,newcol))
-        return moves
+                    self.moves.append((newrow,newcol))
+        return self.moves
 class Queen(Pieces):
     def __init__(self, color, position) -> None:
         super().__init__(color,position)
         self.row, self.col = position
     def __repr__(self) -> str:
-        return f"Q{self.color[0]}"
+        return f"Q {self.color[0].title()}"
     def make_move(self):
-        moves = []
+        self.moves = []
         direction = [(1,1), (1, -1), (-1, 1), (-1, -1)]
         for dcol, drow in direction:
             newrow, newcol = self.row +drow , self.col + dcol
             while 0<newrow<9 and 0<newcol<9:
                 boxpiece = bo.piece_name((newrow,newcol)) 
                 if boxpiece==" ":
-                    moves.append((newrow,newcol))
+                    self.moves.append((newrow,newcol))
                     newrow+=drow
                     newcol+=dcol
                 else:
                     if boxpiece.color== self.color:
-                        moves.append((newrow,newcol))
+                        self.moves.append((newrow,newcol))
                     break
         direction = [(1,0), (1, 0), (0, 1), (0, -1)]
         for dcol, drow in direction:
@@ -95,22 +96,22 @@ class Queen(Pieces):
             while 0<newrow<9 and 0<newcol<9:
                 boxpiece = bo.piece_name((newrow,newcol)) 
                 if boxpiece==" ":
-                    moves.append((newrow,newcol))
+                    self.moves.append((newrow,newcol))
                     newrow+=drow
                     newcol+=dcol
                 else:
                     if boxpiece.color!= self.color:
-                        moves.append((newrow,newcol))
+                        self.moves.append((newrow,newcol))
                     break
-        return moves
+        return self.moves
 class King(Pieces):
     def __init__(self, color, position) -> None:
         super().__init__(color,position)
         self.row, self.col = position
     def __repr__(self) -> str:
-        return f"K{self.color[0]}"
+        return f"K {self.color[0].title()}"
     def make_move(self):
-        moves = []
+        self.moves = []
         direction = []
         direction = [(1,-1),(1,0), (1,1), (0,-1), (0,1), (-1,-1), (-1,0), (-1, 1)]
         for drow, dcol in direction:
@@ -118,40 +119,45 @@ class King(Pieces):
             if(1<=newrow<=8 and 1<=newcol<=8):
                 boxpiece = bo.piece_name((newrow,newcol))
                 if boxpiece==" " or boxpiece.color != self.color:
-                    moves.append((newrow,newcol))
-        return moves
+                    self.moves.append((newrow,newcol))
+        return self.moves
 class Pawn(Pieces):
     def __init__(self, color, position) -> None:
         super().__init__(color,position)
         self.row, self.col = position
         self.moved = False
+        self.unpassant = 0
         self.moves = []
     def __repr__(self) -> str:
-        return f"P{self.color[0]}"
+        return f"P {self.color[0].title()}"
     def make_move(self):
         self.moves = []
         if self.color=="white":
             forward = 1
+            if self.row==4:
+                self.unpassant+=1
             if self.row==5:
                 self.checksides()
         else:
             forward = -1
+            if self.row==5:
+                self.unpassant+=1
             if self.row==4:
                 self.checksides()
-        print(self.row+(1*forward), self.col)
         boxpiece = bo.piece_name((self.row+(1*forward), self.col))
         if boxpiece == " ":
             self.moves.append((self.row+(1*forward), self.col))
         if not self.moved and boxpiece== " " and bo.piece_name((self.row+(2*forward), self.col)) == " ":
             self.moves.append((self.row+(2*forward),self.col))
         for i in (-1,1):
-                boxpiece = bo.piece_name((self.row+1,self.col+i))
-                if boxpiece!=" " and boxpiece.color!=self.color:
-                    self.moves.append((self.row+1,self.col+i))
+                if 0<self.col+i<9:
+                    boxpiece = bo.piece_name((self.row+(1*forward),self.col+i))
+                    if boxpiece!=" " and boxpiece.color!=self.color:
+                        self.moves.append((self.row+(1*forward),self.col+i))
         return self.moves
     def checksides(self):
-            for i in (-1, 1):
+        for i in (-1, 1):
+            if 0<self.col+i<9:
                 boxpiece = bo.piece_name((self.row,self.col+i))
-                if boxpiece!=" " and boxpiece.color!=self.color:
-                    self.moves.append((self.row,self.col+i))
-        
+                if type(boxpiece) ==Pawn and boxpiece.color!=self.color and boxpiece.unpassant==1:
+                    self.moves.append((self.row+1,self.col+i))
