13976f727ead74f691b21ff6ff811cbeb6482987
Yasin Akbarnejad <workyasin2007@gmail.com>
2026-01-03 09:28:17 +0330

    cleaned it

--- legal_move.py
+++ legal_move.py
@@ -1,13 +1,15 @@
 import board
 import pieces as pi
-def is_Legal(pos1, pos2):
+def is_Legal(pos1, pos2, turn):
     pos1, pos2 = board.Translate(pos1), board.Translate(pos2)
     if not(1<=pos1[0]<=8 and 1<=pos1[1]<=8 and 1<= pos2[0]<=8 and 1<=pos2[1]<=8):
             return "Invalid position"
     piece_kind = board.piece_name(pos1)
     if type(piece_kind) in [pi.Rook, pi.Bishop, pi.Knight, pi.Queen, pi.King, pi.Pawn]:
-        if pos2 in piece_kind.moves:
+        if pos2 in piece_kind.moves and piece_kind.color == turn:
             piece_kind.move(pos2)
             return True
+        else:
+            print("problem")
     else:
-        return "Invalid piece"
+        print("Invalid piece")
